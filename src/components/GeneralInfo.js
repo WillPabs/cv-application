a0089f8ebd1548d336a561466d8406cf17da5563
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-12 16:31:07 -0400

    Add editing logic

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,16 +3,74 @@ import React from "react";
 export class GeneralInfo extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            name: this.props.name,
+            email: this.props.email,
+            phoneNo: this.props.phoneNo,
+            isEditing: false
+        }
+    }
+
+    handleChange = (e) => {
+        const property = e.target.id;
+        const value = e.target.value;
+        this.setState({
+            [property]: value
+        })
+    }
+
+    handleEdit = () => {
+        this.setState({
+            isEditing: this.state.isEditing === true ? false : true
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.handleEdit();
     }
 
     render() {
-        const { name, email, phoneNo} = this.props;
-        return(
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-                <p>{phoneNo}</p>
-            </div>
-        );
+        const { name, email, phoneNo, isEditing } = this.state;
+        if (isEditing) {
+            return(
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text"
+                        id="name"
+                        placeholder="Name"
+                        onChange={this.handleChange}
+                        value={name}
+                    />
+                    <input 
+                        type="email"
+                        id="email"
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                        value={email}
+                    />
+                    <input 
+                        type="text"
+                        id="phoneNo"
+                        placeholder="Phone Number"
+                        onChange={this.handleChange}
+                        value={phoneNo}
+                    />
+                    <button>
+                        Resubmit
+                    </button>
+                </form>
+            );
+        } else {
+            return(
+                <div>
+                    <div>{name}</div>
+                    <div id="email">{email}</div>
+                    <div id="phoneNo">{phoneNo}</div>
+                    <button onClick={this.handleEdit}>Edit</button>
+                </div>
+            );
+        }
     };
 }
