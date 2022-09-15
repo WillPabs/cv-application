059ab30dd01514bcfff8dee82b54756cb06e70aa
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-15 13:57:17 -0400

    Abstract logic of multiple Professional components to ProfessionalSection

--- src/components/Professional.js
+++ src/components/Professional.js
@@ -5,26 +5,15 @@ export class Professional extends React.Component {
         super(props);
 
         this.state = {
-            experiences: this.props.experiences,
-            id: '',
-            companyName: '',
-            positionTitle: '',
-            tasks: '',
-            dateStarted: '',
-            dateEnded: '',
-            isEditing: true,
+            id: this.props.id,
+            companyName: this.props.companyName,
+            positionTitle: this.props.positionTitle,
+            tasks: this.props.tasks,
+            dateStarted: this.props.dateStarted,
+            dateEnded: this.props.dateEnded,
+            isEditing: this.props.isEditing,
         }
     };
-
-    updateObjInArray = (e) => {
-        const property = e.target.id;
-        const value = e.target.value;
-        this.setState(current => {
-            current.map(obj => {
-                return {...obj, [property]: value};
-            });
-        });
-    }
 
     handleChange = (e) => {
         const property = e.target.id;
@@ -46,89 +35,71 @@ export class Professional extends React.Component {
         e.preventDefault();
         this.handleEdit();
         console.log(this.state)
-        this.props.experiences = [...this.props.experiences, 
-                {
-                    id: this.state.id,
-                    companyName: this.state.companyName,
-                    positionTitle: this.state.positionTitle,
-                    tasks: this.state.tasks,
-                    dateStarted: this.state.dateStarted,
-                    dateEnded: this.state.dateEnded,
-                    isEditing: false,
-                }
-            ]
-
+        this.setState({
+            id: this.state.id,
+            companyName: this.state.companyName,
+            positionTitle: this.state.positionTitle,
+            tasks: this.state.tasks,
+            dateStarted: this.state.dateStarted,
+            dateEnded: this.state.dateEnded,
+            isEditing: false,
+        })
     }
 
     render() {
-        const { experiences } = this.props;
-        console.log(experiences)
-        const heading = <div>Professional Experience</div>;
-        const { addExperience } = this.props;
-        return (
-            <ul>
-                {heading}
-                {experiences.map((experience, i) => {
-                    const { id, companyName, positionTitle, tasks, dateStarted, dateEnded, isEditing } = experience;
-                
-                    if (isEditing) {
-                        // const { id, companyName, positionTitle, tasks, dateStarted, dateEnded, isEditing } = this.state;
-                        return(
-                            <form key={i} id={id} onSubmit={this.handleSubmit}>
-                                <input 
-                                    type="text"
-                                    id="companyName"
-                                    placeholder="Company Name"
-                                    onChange={this.handleChange}
-                                    value={companyName}
-                                />
-                                <input 
-                                    type="text"
-                                    id="positionTitle"
-                                    placeholder="Position Title"
-                                    onChange={this.handleChange}
-                                    value={positionTitle}
-                                />
-                                <input 
-                                    type="text"
-                                    id="tasks"
-                                    placeholder="Tasks"
-                                    onChange={this.handleChange}
-                                    value={tasks}
-                                />
-                                <input 
-                                    type="text"
-                                    id="dateStarted"
-                                    placeholder="Date Started"
-                                    onChange={this.handleChange}
-                                    value={dateStarted}
-                                />
-                                <input 
-                                    type="text"
-                                    id="dateEnded"
-                                    placeholder="Date Ended"
-                                    onChange={this.handleChange}
-                                    value={dateEnded}
-                                />
-                                <button>Submit</button>
-                            </form>
-                        );
-                    } else {
-                        return(
-                            <div key={id} id={id}>
-                                {heading}
-                                <div>{companyName}</div>
-                                <div>{positionTitle}</div>
-                                <div>{tasks}</div>
-                                <div>{dateStarted}</div>
-                                <div>{dateEnded}</div>
-                                <button onClick={this.handleEdit}>Edit</button>
-                            </div>
-                        );
-                    }
-                })}
-                <button onClick={addExperience}>Add Experience</button>
-            </ul>
-        )
-    };
+        const { id, companyName, positionTitle, tasks, dateStarted, dateEnded, isEditing } = this.state;
+        if (isEditing) {
+            return(
+                <form key={id} id={id} onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text"
+                        id="companyName"
+                        placeholder="Company Name"
+                        onChange={this.handleChange}
+                        value={companyName}
+                    />
+                    <input 
+                        type="text"
+                        id="positionTitle"
+                        placeholder="Position Title"
+                        onChange={this.handleChange}
+                        value={positionTitle}
+                    />
+                    <input 
+                        type="text"
+                        id="tasks"
+                        placeholder="Tasks"
+                        onChange={this.handleChange}
+                        value={tasks}
+                    />
+                    <input 
+                        type="text"
+                        id="dateStarted"
+                        placeholder="Date Started"
+                        onChange={this.handleChange}
+                        value={dateStarted}
+                    />
+                    <input 
+                        type="text"
+                        id="dateEnded"
+                        placeholder="Date Ended"
+                        onChange={this.handleChange}
+                        value={dateEnded}
+                    />
+                    <button>Submit</button>
+                </form>
+            );
+        } else {
+            return(
+                <div key={id} id={id}>
+                    <div>{companyName}</div>
+                    <div>{positionTitle}</div>
+                    <div>{tasks}</div>
+                    <div>{dateStarted}</div>
+                    <div>{dateEnded}</div>
+                    <button onClick={this.handleEdit}>Edit</button>
+                </div>
+            );
+        }
+    }
 };
