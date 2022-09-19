4ce3a2d733d3bd513cc9e6485958df84d04b32c3
Will <pabiterowilliam@yahoo.com>
2022-09-19 01:40:22 -0400

    Add tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,23 @@
+import React from "react";
+import uniqid from "uniqid";
+
+export class Tasks extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            tasks: [],
+            task: {
+                id: uniqid(),
+                text: ''
+            }
+        }
+    }
+
+    render() {
+        const 
+        return (
+
+        )
+    }
+}
