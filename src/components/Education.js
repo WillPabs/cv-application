eea8bf69c50775ad45a92207476871e147ed2a50
Will Pabs <pabiterowilliam@yahoo.com>
2022-10-17 14:51:50 -0400

    Refactor to use functional components and hooks

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,76 +1,64 @@
 import React from "react";
+import { useState } from "react";
 
-export class Education extends React.Component {
-    constructor(props) {
-        super(props);
+export const Education = (props) => {
+    const [id, setId] = useState(props.id);
+    const [schoolName, setSchoolName] = useState(props.schoolName);
+    const [major, setMajor] = useState(props.major);
+    const [dateOfStudy, setDateOfStudy] = useState(props.dateOfStudy);
+    const [isEditing, setIsEditing] = useState(props.isEditing);
 
-        this.state = {
-            id: this.props.id,
-            schoolName: this.props.schoolName,
-            major: this.props.major,
-            dateOfStudy: this.props.dateOfStudy,
-            isEditing: this.props.isEditing
-        }
-    };
-
-    handleChange = (e) => {
+    const handleChange = (e) => {
         const property = e.target.id;
         const value = e.target.value;
-        this.setState({
-            [property]: value
-        })
-    }
 
-    handleEdit = () => {
-        this.setState({
-            isEditing: this.state.isEditing === true ? false : true
-        });
+        if (property === 'id') setId(value)
+        else if (property === 'schoolName') setSchoolName(value)
+        else if (property === 'major') setMajor(value)
+        else if (property === 'dateOfStudy') setDateOfStudy(value)
     }
-
-    handleSubmit = (e) => {
+    
+    const handleEdit = (e) => {
         e.preventDefault();
-        this.handleEdit();
+        setIsEditing(() => isEditing === true ? false : true)
     }
 
-    render() {
-        const { id, schoolName, major, dateOfStudy, isEditing } = this.state;
-        if (isEditing) {
-            return (
-                <form id={id} onSubmit={this.props.deleteExperience}>
-                    <input
-                        type="text"
-                        id="schoolName"
-                        placeholder="School Name"
-                        value={schoolName}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        id="major"
-                        placeholder="Major"
-                        value={major}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        id="dateOfStudy"
-                        placeholder="Date of Study"
-                        value={dateOfStudy}
-                        onChange={this.handleChange}
-                    />
-                    <button className="button-edit" onClick={this.handleSubmit}>Resubmit</button>
-                    <button className="button-delete" type="submit">Delete Experience</button>
-                </form>
-            );
-        } else {
-            return (
-                <div>
-                    <div>{schoolName}</div>
-                    <div>{major}</div>
-                    <div>{dateOfStudy}</div>
-                    <button className="button-edit" onClick={this.handleEdit}>Edit</button>
-                </div>
-            );
-        }
-    };
-};
+    if (isEditing) {
+        return (
+            <form id={id} onSubmit={props.deleteExperience}>
+                <input
+                    type="text"
+                    id="schoolName"
+                    placeholder="School Name"
+                    value={schoolName}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    id="major"
+                    placeholder="Major"
+                    value={major}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    id="dateOfStudy"
+                    placeholder="Date of Study"
+                    value={dateOfStudy}
+                    onChange={handleChange}
+                />
+                <button className="button-edit" onClick={handleEdit}>Resubmit</button>
+                <button className="button-delete" type="submit">Delete Experience</button>
+            </form>
+        );
+    } else {
+        return (
+            <div>
+                <div>{schoolName}</div>
+                <div>{major}</div>
+                <div>{dateOfStudy}</div>
+                <button className="button-edit" onClick={handleEdit}>Edit</button>
+            </div>
+        );
+    }
+}
