559dde8f0bc95c77ef9ef21f333f2f20e905579d
Will <pabiterowilliam@yahoo.com>
2022-10-17 18:09:40 -0400

    Refactor to use functional components and hooks

--- src/components/ProfessionalSection.js
+++ src/components/ProfessionalSection.js
@@ -1,77 +1,140 @@
 import React from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 import { Professional } from "./Professional";
 
-export class ProfessionalSection extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            experiences: [],
-            experience: {
-                id: uniqid(),
-                companyName: '',
-                positionTitle: '',
-                dateStarted: '',
-                dateEnded: '',
-                isEditing: true,
-                tasks: []
-            },
+export const ProfessionalSection = () => {
+    const [experiences, setExperiences] = useState([]);
+    const [experience, setExperience] = useState(
+        {
+            id: uniqid(),
+            companyName: '',
+            positionTitle: '',
+            dateStarted: '',
+            dateEnded: '',
+            isEditing: true,
+            tasks: []
         }
+    )
+
+    const addExperience = () => {
+        setExperiences([...experiences, experience]);
+        setExperience({
+            id: uniqid(),
+            companyName: '',
+            positionTitle: '',
+            dateStarted: '',
+            dateEnded: '',
+            isEditing: true,
+            tasks: []
+        })
     };
 
-    addExperience = () => {
-        this.setState({
-            experiences: [...this.state.experiences, this.state.experience],
-            experience: {
-                id: uniqid(),
-                companyName: '',
-                positionTitle: '',
-                tasks: [],
-                dateStarted: '',
-                dateEnded: '',
-                isEditing: true,
-            },
-        });
-    };
-
-    deleteExperience = (e) => {
+    const deleteExperience = (e) => {
         e.preventDefault();
         const id = e.target.id;
-        this.setState({
-            experiences: this.state.experiences.filter(experience => experience.id !== id)
-        });
-        return false;
+        experiences.filter(experience => experience.id !== id)
     };
 
-    render() {
-        const { experiences } = this.state;
-        const heading = <div className="section-heading">Professional Experience</div>;
-        return(
-            <ul>
-                {heading}
-                <button 
-                    className="button-add"
-                    onClick={this.addExperience}
-                >
-                    Add Experience
-                </button>
-                {experiences.map((experience) => {
-                    return (
-                        <Professional
-                            key={experience.id} 
-                            id={experience.id}
-                            companyName={experience.companyName}
-                            positionTitle={experience.positionTitle}
-                            tasks={experience.tasks}
-                            dateStarted={experience.dateStarted}
-                            dateEnded={experience.dateEnded}
-                            isEditing={experience.isEditing}
-                            deleteExperience={this.deleteExperience}
-                        />
-                    )
-                })}
-            </ul>
-        )
-    }
+    const heading = <div className="section-heading">Professional Experience</div>;
+    return(
+        <ul>
+            {heading}
+            <button 
+                className="button-add"
+                onClick={addExperience}
+            >
+                Add Experience
+            </button>
+            {experiences.map((experience) => {
+                return (
+                    <Professional
+                        key={experience.id} 
+                        id={experience.id}
+                        companyName={experience.companyName}
+                        positionTitle={experience.positionTitle}
+                        tasks={experience.tasks}
+                        dateStarted={experience.dateStarted}
+                        dateEnded={experience.dateEnded}
+                        isEditing={experience.isEditing}
+                        deleteExperience={deleteExperience}
+                    />
+                )
+            })}
+        </ul>
+    )
 }
+
+// export class ProfessionalSection extends React.Component {
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             experiences: [],
+//             experience: {
+//                 id: uniqid(),
+//                 companyName: '',
+//                 positionTitle: '',
+//                 dateStarted: '',
+//                 dateEnded: '',
+//                 isEditing: true,
+//                 tasks: []
+//             },
+//         }
+//     };
+
+//     addExperience = () => {
+//         this.setState({
+//             experiences: [...this.state.experiences, this.state.experience],
+//             experience: {
+//                 id: uniqid(),
+//                 companyName: '',
+//                 positionTitle: '',
+//                 tasks: [],
+//                 dateStarted: '',
+//                 dateEnded: '',
+//                 isEditing: true,
+//             },
+//         });
+//     };
+
+//     deleteExperience = (e) => {
+//         e.preventDefault();
+//         const id = e.target.id;
+//         this.setState({
+//             experiences: this.state.experiences.filter(experience => experience.id !== id)
+//         });
+//         return false;
+//     };
+
+//     render() {
+//         const { experiences } = this.state;
+//         const heading = <div className="section-heading">Professional Experience</div>;
+//         return(
+//             <ul>
+//                 {heading}
+//                 <button 
+//                     className="button-add"
+//                     onClick={this.addExperience}
+//                 >
+//                     Add Experience
+//                 </button>
+//                 {experiences.map((experience) => {
+//                     return (
+//                         <Professional
+//                             key={experience.id} 
+//                             id={experience.id}
+//                             companyName={experience.companyName}
+//                             positionTitle={experience.positionTitle}
+//                             tasks={experience.tasks}
+//                             dateStarted={experience.dateStarted}
+//                             dateEnded={experience.dateEnded}
+//                             isEditing={experience.isEditing}
+//                             deleteExperience={this.deleteExperience}
+//                         />
+//                     )
+//                 })}
+//             </ul>
+//         )
+//     }
+// }
